7de54a6aa3961e8093208dec399cd75609b93c05
honako22 <106946647+honako22@users.noreply.github.com>
2022-06-08 10:54:37 +0700

    Remove WingStepToken.sol pausable

--- WingStepToken.sol
+++ WingStepToken.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.10;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
@@ -12,25 +11,8 @@ contract WingStepToken is Initializable, ERC20Upgradeable, PausableUpgradeable, 
 
     function initialize() initializer external {
         __ERC20_init("WingStep Token", "WST");
-        __Pausable_init();
         __Ownable_init();
 
         _mint(msg.sender, 0.5 * 1e9 * 10 ** decimals());
-    }
-
-    function pause() external onlyOwner {
-        _pause();
-    }
-
-    function unpause() external onlyOwner {
-        _unpause();
-    }
-
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
-        internal
-        whenNotPaused
-        override
-    {
-        super._beforeTokenTransfer(from, to, amount);
     }
 }
