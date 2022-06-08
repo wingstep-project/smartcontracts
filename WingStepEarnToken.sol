3e703c029fe03f0a8b04af1d88af8bc14a9ed325
honako22 <106946647+honako22@users.noreply.github.com>
2022-06-08 11:01:06 +0700

    Remove pausable

--- WingStepEarnToken.sol
+++ WingStepEarnToken.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20Burnable
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract WingStepEarnToken is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, PausableUpgradeable, OwnableUpgradeable {
+contract WingStepEarnToken is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, OwnableUpgradeable {
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() initializer {}
 
