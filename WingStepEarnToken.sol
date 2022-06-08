c298c180b40c7ab05a6c1aa4645c15c93ab2b412
honako22 <106946647+honako22@users.noreply.github.com>
2022-06-08 10:55:27 +0700

    Remove WingStepEarnToken.sol pausable

--- WingStepEarnToken.sol
+++ WingStepEarnToken.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.10;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
@@ -14,27 +13,11 @@ contract WingStepEarnToken is Initializable, ERC20Upgradeable, ERC20BurnableUpgr
     function initialize() initializer external {
         __ERC20_init("WingStep Earn Token", "WSS");
         __ERC20Burnable_init();
-        __Pausable_init();
         __Ownable_init();
-    }
-
-    function pause() external onlyOwner {
-        _pause();
-    }
-
-    function unpause() external onlyOwner {
-        _unpause();
     }
 
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
-        internal
-        whenNotPaused
-        override
-    {
-        super._beforeTokenTransfer(from, to, amount);
-    }
 }
